6fafbe54a9526058b29c126edb5ba0f118ffee9e
Andreas Blaesius <skate4life@gmx.de>
2017-03-10 12:44:01 +0100

    Use scripts to zip the kernel packages

---
+++ make_anykernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DATE=`eval date +%Y`-`eval date +%m`-`eval date +%d`
+PACKAGENAME=Anykernel-$DATE
+
+if [ -f $PACKAGENAME.zip ]; then
+	echo "$PACKAGENAME exist already... skipping."
+else
+	zip -r $PACKAGENAME.zip META-INF/ modules/ tools/ anykernel.sh zImage
+fi
