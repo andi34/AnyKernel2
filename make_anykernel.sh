79952c165504da5e717eb76ac87e93d067eab006
Andreas Blaesius <skate4life@gmx.de>
2018-01-26 22:24:07 +0100

    Use anykernel.sh depending on supported Android version(s)

--- make_anykernel.sh
+++ make_anykernel.sh
@@ -2,6 +2,10 @@
 
 DATE=`eval date +%Y`-`eval date +%m`-`eval date +%d`-`eval date +%H`-`eval date +%M`
 
+if [ -f ./tmp/anykernel.sh ]; then
+	cp -fr ./tmp/anykernel.sh ./anykernel.sh
+fi
+
 if [[ ! -z $1 ]]; then
 	PACKAGENAME=AnyKernel-$DATE-espresso-$1
 else
@@ -12,4 +16,9 @@ if [ -f $PACKAGENAME.zip ]; then
 	echo "$PACKAGENAME exist already... skipping."
 else
 	zip -r $PACKAGENAME.zip META-INF/ modules/ tools/ anykernel.sh zImage
+fi
+
+if [ -f ./anykernel.sh ]; then
+	echo "cleanup"
+	rm -rf ./anykernel.sh
 fi
---
+++ tmp/anykernel-kk.sh
@@ -0,0 +1,63 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=Android-Andi @ xda-developers
+do.devicecheck=1
+do.modules=1
+do.cleanup=0
+do.cleanuponabort=0
+device.name1=espresso
+device.name2=espresso3g
+device.name3=espresso-common
+device.name4=espressowifi
+device.name5=p3100
+device.name6=p3110
+device.name7=p3113
+device.name8=p5100
+device.name9=p5110
+device.name10=p5113
+device.name11=espressowifixx
+device.name12=espresso10wifixx
+device.name13=espressorf
+device.name14=espressorfxx
+device.name15=espresso10rf
+device.name16=espresso10rfxx
+} # end properties
+
+# shell variables
+block=/dev/block/platform/omap/omap_hsmmc.1/by-name/KERNEL;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## Alert of unsupported Android version
+android_ver=$(mount /system; grep "^ro.build.version.release" /system/build.prop | cut -d= -f2; umount /system);
+case "$android_ver" in
+  "4.4.4") support_status="supported";;
+  *) support_status="unsupported";;
+esac;
+ui_print "Running Android $android_ver..."
+ui_print "This kernel is $support_status for this version!";
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+backup_file init.omap4.rc;
+insert_line init.omap4.rc "omaplfb.ko" before "insmod /system/lib/modules/pvrsrvkm.ko" "    insmod /system/lib/modules/omaplfb.ko";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
